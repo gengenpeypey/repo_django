0c9a07ca92351e10654cd58b759b7d5a357a2634
pay <71933183+gengenpeypey@users.noreply.github.com>
2021-09-08 02:37:48 +0900

    lesson06

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = 'mamazon'
+detail = 'product-detail'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
